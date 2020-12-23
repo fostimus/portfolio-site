4b0794b8281cca85fb9b3e54dce45217a9b84c91
Derek Foster <derekfoster94@gmail.com>
2020-12-23 14:35:58 -0800

    added bulma

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-/**
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
- */
-
 require("dotenv").config();
 const express = require("express"); // Express web server framework
-// HTTP client; deprecated feb 11, 2020. Should use something
+// HTTP client; deprecated feb 11, 2020. Should use something (e.g. axios)
 const request = require("request"); // "Request" library
 const cors = require("cors");
 const querystring = require("querystring");
