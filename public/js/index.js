175d689a2b32ca8495def5590c503828df7df1b5
Derek Foster <derekfoster94@gmail.com>
2020-12-23 16:40:03 -0800

    used client side js to enable expanding divs, along with css to show skills in categories

--- public/js/index.js
+++ public/js/index.js
@@ -12,3 +12,31 @@ projects.forEach(project => {
       project.querySelector("p").textContent = data.description;
     });
 });
+
+/**
+ * skills manipulation
+ */
+
+let originalSkillHeight = null;
+const skills = document.querySelectorAll(".skill");
+
+//TODO: also allow for clicking to expand? for mobile
+skills.forEach(skill => {
+  skill.addEventListener("mouseenter", () => {
+    if (originalSkillHeight === null) {
+      originalSkillHeight = skill.offsetHeight;
+    }
+
+    const subSkills = skill.querySelectorAll(".skill-item");
+    // +20 is padding
+    const subSkillsHeight = subSkills[0].offsetHeight * subSkills.length + 20;
+
+    const newHeight = skill.offsetHeight + subSkillsHeight;
+
+    skill.setAttribute("style", "height: " + newHeight.toString() + "px");
+  });
+
+  skill.addEventListener("mouseleave", () => {
+    skill.setAttribute("style", "height: " + originalSkillHeight + "px");
+  });
+});
