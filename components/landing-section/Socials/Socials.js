5699c4a6649b1b0d28bb5ac9d14445fb526091a8
Derek Foster <derekfoster94@gmail.com>
2021-01-21 21:49:45 -0800

    finished socials

--- components/landing-section/Socials/Socials.js
+++ components/landing-section/Socials/Socials.js
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import Image from "next/image";
+import styles from "./socials.module.scss";
 
-function Social({ info, black }) {
+function Social({ info }) {
   const [mouseEnter, setMouseEnter] = useState(false);
-
-  console.log(info);
 
   return (
     <a
@@ -12,61 +11,23 @@ function Social({ info, black }) {
       target="_blank"
       onMouseEnter={() => setMouseEnter(true)}
       onMouseLeave={() => setMouseEnter(false)}
+      key={info.name}
     >
       {mouseEnter ? (
         <Image src={info.hover} width={25} height={25} />
-      ) : black ? (
-        <Image src={info.black} width={25} height={25} />
       ) : (
-        <Image src={info.white} width={25} height={25} />
+        <Image src={info.icon} width={25} height={25} />
       )}
     </a>
   );
 }
 
-export default function Socials({ socials, black }) {
+export default function Socials({ socials }) {
   return (
-    <section className="social-links <%= background %>">
+    <section className={styles["social-links"]}>
       {socials.map(social => (
-        <Social info={social} black={black} />
+        <Social info={social} />
       ))}
     </section>
   );
 }
-
-/*
-<Social black />
-<a href="https://www.linkedin.com/in/derek-foster/" target="_blank">
-  <img
-    src="images/social-media-icons/<%= color %>/linkedin.svg"
-    onmouseover="this.src='images/social-media-icons/hover/linkedin.svg'"
-    onmouseout="this.src='images/social-media-icons/<%= color %>/linkedin.svg'"
-  />
-</a>
-<a href="https://github.com/fostimus" target="_blank">
-  <img
-    src="images/social-media-icons/<%= color %>/github.svg"
-    onmouseover="this.src='images/social-media-icons/hover/github.svg'"
-    onmouseout="this.src='images/social-media-icons/<%= color %>/github.svg'"
-  />
-</a>
-<a
-  href="https://open.spotify.com/user/1218307071?si=Ir3WJzehS9qQe-k_gaJpng"
-  target="_blank"
->
-  <img
-    src="images/social-media-icons/<%= color %>/spotify.svg"
-    onmouseover="this.src='images/social-media-icons/hover/spotify.svg'"
-    onmouseout="this.src='images/social-media-icons/<%= color %>/spotify.svg'"
-  />
-</a>
-<a
-  href="https://www.yelp.com/user_details?userid=ktbe2eLWnfgu3afRmDIi3g"
-  target="_blank"
->
-  <img
-    src="images/social-media-icons/<%= color %>/yelp.svg"
-    onmouseover="this.src='images/social-media-icons/hover/yelp.svg'"
-    onmouseout="this.src='images/social-media-icons/<%= color %>/yelp.svg'"
-  />
-</a>*/
---
+++ pages/about.js
@@ -0,0 +1,24 @@
+// import Layout from "../components/Layout";
+import SkillContainer from "../components/landing-section/SkillContainer";
+import Socials from "../components/landing-section/Socials";
+import { skillsParser } from "../content/markdown-parser";
+import { whiteSocials } from "../content/socials";
+
+export async function getStaticProps() {
+  const allSkillsData = await skillsParser();
+  return {
+    props: {
+      allSkillsData,
+      socials: whiteSocials
+    }
+  };
+}
+
+export default function AboutPage({ allSkillsData, socials }) {
+  return (
+    <>
+      <Socials socials={socials} />
+      <SkillContainer skills={allSkillsData} />
+    </>
+  );
+}
