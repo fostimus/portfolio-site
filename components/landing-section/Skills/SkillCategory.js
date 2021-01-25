360184d8dcf5015b55cac69dcb0bfc446539da9b
Derek Foster <derekfoster94@gmail.com>
2021-01-25 13:23:08 -0800

    fine tuned skill expansion height

--- components/landing-section/Skills/SkillCategory.js
+++ components/landing-section/Skills/SkillCategory.js
@@ -4,12 +4,15 @@ import { useState } from "react";
 export default function SkillCategory({ name, skills }) {
   const [mouseEnter, setMouseEnter] = useState(false);
 
+  const skillCategoryHeight =
+    skills.length > 5
+      ? styles["skill-height-" + skills.length]
+      : styles["skill-height-" + (skills.length - 1)];
+
   return (
     <div
       className={
-        mouseEnter
-          ? `${styles.skill} ${styles["skill-height-" + skills.length]}`
-          : styles.skill
+        mouseEnter ? `${styles.skill} ${skillCategoryHeight}` : styles.skill
       }
       onMouseEnter={() => setMouseEnter(true)}
       onMouseLeave={() => setMouseEnter(false)}
