85a01b0ff98b8886193f3064e9f026f43820ca3c
Derek Foster <derekfoster94@gmail.com>
2021-03-03 10:52:25 -0800

    rotating text but no flip

--- components/background/BackgroundSection/BackgroundSection.js
+++ components/background/BackgroundSection/BackgroundSection.js
@@ -10,32 +10,34 @@ export default function BackgroundSection({
   children
 }) {
   return (
-    <div className={styles.flip}>
-      <Card className={styles.cardFront}>
-        <Card.Title>{title}</Card.Title>
-      </Card>
-      <Card
-        className={`${className} ${styles["bg-section"]} ${styles.cardBack}`}
-      >
-        {typeof content === "string" ? (
-          <Card.Body>
-            <Card.Title>{title}</Card.Title>
-            <Card.Subtitle>{subtitle}</Card.Subtitle>
-            <div
-              className={styles.body}
-              dangerouslySetInnerHTML={{ __html: content }}
-            ></div>
-          </Card.Body>
-        ) : (
-          <>
-            {title && <Card.Title>{title}</Card.Title>}
-            {subtitle && <Card.Subtitle>{subtitle}</Card.Subtitle>}
-            <Card.Body className={vertical && styles.vertical}>
-              {children}
+    <div
+      className={`${className} ${styles["bg-section"]} ${styles["flip-card"]}`}
+    >
+      <div className={styles.flip}>
+        <Card className={styles.cardFront}>
+          <Card.Title>{title}</Card.Title>
+        </Card>
+        <Card className={` ${styles.cardBack}`}>
+          {typeof content === "string" ? (
+            <Card.Body>
+              <Card.Title>{title}</Card.Title>
+              <Card.Subtitle>{subtitle}</Card.Subtitle>
+              <div
+                className={styles.body}
+                dangerouslySetInnerHTML={{ __html: content }}
+              ></div>
             </Card.Body>
-          </>
-        )}
-      </Card>
+          ) : (
+            <>
+              {title && <Card.Title>{title}</Card.Title>}
+              {subtitle && <Card.Subtitle>{subtitle}</Card.Subtitle>}
+              <Card.Body className={vertical && styles.vertical}>
+                {children}
+              </Card.Body>
+            </>
+          )}
+        </Card>
+      </div>
     </div>
   );
 }
